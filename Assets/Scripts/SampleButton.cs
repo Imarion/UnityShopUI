5a4f3e4691abaccb639da3deaeb334d23763ac43
emr <renquin@awtce.be>
2019-07-19 14:04:30 +0200

    Up to 5/9 3:23, pooled object to be completed.

---
+++ Assets/Scripts/SampleButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour
+{
+
+    public Button button;
+    public Text nameLabel;
+    public Text priceLabel;
+    public Image iconImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setup()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SimpleObjectPool.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectPool : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    private Stack<GameObject> inactiveInstances = new Stack<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject GetObject()
+    {
+        GameObject spawnedGameObject;
+
+        if (inactiveInstances.Count > 0)
+        {
+            spawnedGameObject = inactiveInstances.Pop();
+        }
+        else
+        {
+            spawnedGameObject = (GameObject)GameObject.Instantiate(prefab);
+            PooledObject pooledObject = spawnedGameObject.AddComponent<PooledObject>();
+            pooledObject.pool = this;
+        }
+
+        spawnedGameObject.transform.SetParent(null);
+        spawnedGameObject.SetActive(true);
+
+        return spawnedGameObject;
+    }
+
+    public void ReturnObject(GameObject toReturn)
+    {
+        PooledObject pooledObject = toReturn.GetComponent<PooledObject>();
+
+        if (pooledObject != null && pooledObject.pool == this)
+        {
+            toReturn.transform.SetParent(transform);
+            toReturn.SetActive(false);
+            inactiveInstances.Push(toReturn);
+        }
+        else
+        {
+            Debug.LogWarning(toReturn.name + " was returned to a poll it wasn't spawned from! Destroying.");
+            Destroy(toReturn);
+        }
+    }
+
+    public class PooledObject
+    {
+    }
+}
